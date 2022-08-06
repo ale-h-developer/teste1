abc727411d21e779d7745c7570f3ecc9885e62dc
Alessandra.h <alessandrahartkopfdeveloper@gmail.com>
2022-08-06 00:39:42 -0300

    changed files add style and js

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # teste1
 Teste 1
+Readme file 1.
---
+++ script.js
@@ -0,0 +1,9 @@
+alert("Hello World!!!");
+
+let rt = document.getElementsByClassName("root");
+
+rt.addEventListener("click", myFunction);
+
+function myFunction() {
+    backgroundColor = "red";
+}
