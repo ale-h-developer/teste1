a0b8b67a59c6e88ce666a4a3becef5ab053c09b3
Alessandra.h <alessandrahartkopfdeveloper@gmail.com>
2022-08-09 00:40:14 -0300

    changed page

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
-alert("Hello World!!!");
+
 
 let myContainer = document.getElementById("container");
+let myButton = document.getElementById("myButton");
+let myData = document.getElementById("myInput");
+let para = document.getElementById("demo");
 
-function myFunction() {
-    console.log("Hello you!");
-    myContainer.innerHTML = "Hello Everybody!"
-    myContainer.style.backgroundColor = "red";
+// function myFunction() {
+//     console.log("Hello you!");
+//     myContainer.innerHTML = "Hello Everybody!"
+    
+// }
+
+function myFunction2() {
+    if (myData.value !== "") {
+    para.innerText = `Hello ${myData.value} !!!`
+    myContainer.style.background = "black";
+    } else {
+        para.innerText = "Hello you!"
+    }
 }
 
-myContainer.addEventListener("click", myFunction);
+// myContainer.addEventListener("click", myFunction);
+myButton.addEventListener("click", myFunction2);
 
